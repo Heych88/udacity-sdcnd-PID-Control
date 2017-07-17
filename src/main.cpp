15fe7bd94f9c7be63efceb11448adf5207c73896
haidyn <haidyn1988@gmail.com>
2017-07-18 09:34:43 +1000

    better parameters

--- src/main.cpp
+++ src/main.cpp
@@ -31,10 +31,13 @@ std::string hasData(std::string s) {
 int main()
 {
   uWS::Hub h;
-
+  
   PID pid;
   // TODO: Initialize the pid variable.
-  pid.Init(0.2, 0.002, 10);
+  double kp = 0.13;
+  double ki = 0.002;
+  double kd = 2.5;
+  pid.Init(kp, ki, kd);
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -59,7 +62,10 @@ int main()
           * another PID controller to control the speed!
           */
           pid.UpdateError(cte); 
-          steer_value = pid.output; 
+          steer_value = -pid.output;
+          
+          if(steer_value > 1) steer_value = 1;
+          else if(steer_value < -1) steer_value = -1;
           
           // DEBUG
           std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
